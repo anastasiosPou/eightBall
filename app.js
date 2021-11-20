c7f96d23d1d3a113f4c21fc7efd4a56ff14943e2
Tasos <tasos.poursaitidis.4@gmail.com>
2021-11-20 11:35:29 +0200

    Write the function to generate the random answer

--- app.js
+++ app.js
@@ -3,21 +3,40 @@ const answers = require('./data.js');
 
 console.log('This is Eight-Ball. You should ask a Yes/No question.');
 
-// //Capture the user's question
-// const readline = require('readline').createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-//   })
-
-// let userQuestion = '';
-  
-// readline.question(`Ask me anything:`, question => {
-//   userQuestion = question;
-//   readline.close();
-// });
-
-// console.log(`The question was -> ${userQuestion}`);
 var prompt = require('prompt-sync')();
 
+//Store the user's question
 var userQuestion = prompt('Ask:');
-console.log(`You asked: ${userQuestion}`);
+
+//Destructure the 3 arrays with the answers for later use
+const {positive: positiveAnswers, neutral: neutralAnswers, negative: negativeAnswers} = answers;
+
+function generateRandomAnswer() {
+  //Function to choose a random number
+  const randomize = upperLimit => Math.floor(Math.random() * upperLimit);
+
+  const randomNumber = randomize(3);
+  let answer = '';
+  let randomArray = [];
+
+  //Choose randomly an array
+  switch(randomNumber) {
+    case 0:
+      randomArray = positiveAnswers;
+      break;
+    case 1:
+      randomArray = neutralAnswers;
+      break;
+    case 2:
+      randomArray = negativeAnswers;
+      break;
+  }
+
+  //We choose again a random answer from the array we've randomly chosen
+  answer = randomArray[randomize(randomArray.length)];
+  
+  console.log(`My answer: ${answer}`);
+  
+}
+
+generateRandomAnswer();
